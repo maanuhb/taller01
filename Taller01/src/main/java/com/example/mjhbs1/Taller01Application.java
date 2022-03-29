fdf4166b30b8e1590f19c8fd4f2fdaa312837562
maanuhb <00094119@uca.edu.sv>
2022-03-28 21:04:09 -0600

    First commit
    Add first commit

---
+++ Taller01/src/main/java/com/example/mjhbs1/Taller01Application.java
@@ -0,0 +1,22 @@
+package com.example.mjhbs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@SpringBootApplication
+public class Taller01Application {
+	
+	@RequestMapping(value="/")
+	public String index() {
+		return "<h1> Holiwis <h1>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller01Application.class, args);
+	}
+
+}
---
+++ Taller01/src/test/java/com/example/mjhbs1/Taller01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mjhbs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
