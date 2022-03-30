ed0c887d8ced651df460700bf162959e650ca8bf
Cubo QA <cuboqa@Cubos-MacBook-Pro.local>
2022-03-30 00:40:03 -0600

    UserController

--- Taller01/src/main/java/com/example/mjhbs1/controllers/UserController.java
+++ Taller01/src/main/java/com/example/mjhbs1/controllers/UserController.java
@@ -5,7 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller ("/taller")
 public class UserController  {
 	@PostMapping("/validator")
-	public String showuserdata (@Valid dep, name, lastname, numberId, birthdate ) {
-		
+
+	public String validate(
+			@RequestParam("dep") String dep,
+			@RequestParam("name") String name,
+			@RequestParam("lastName") String lastName,
+			@RequestParam("idNumber") String idNumber,
+			@RequestParam("birthdate") String birthdate, ModelMap modelMap) {
+		if (Util.isDateValid(birthdate)) {
+			System.out.println("valid Birthdate");
+		}
+		modelMap.put("dep", dep);
+		modelMap.put("name", name);
+		modelMap.put("lastName", lastName);
+		modelMap.put("idNumber", idNumber);
+		modelMap.put("birthdate", birthdate);
 	}
-}
+	 	
+	}
+
